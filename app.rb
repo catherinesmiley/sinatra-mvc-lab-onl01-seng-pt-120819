730a32a2a35b63cb1a1a7c18978a343c0ce43283
cesmiley <catherinesmiley@gmail.com>
2020-03-25 18:50:26 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,16 +2,13 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-    get "/" do 
+    get '/' do
         erb :user_input
-    end 
-
-    post "/" do 
-        @pig_latinized = PigLatinizer.new(params[:user_phrase])
-    end 
-
-    post "/piglatinize" do 
-
     end
 
+    post '/piglatinize' do
+        converter = PigLatinizer.new
+        converter.piglatinize(params[:user_phrase])
+    end
+    
 end
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,23 @@
-class PigLatinizer 
+class PigLatinizer
 
-    def piglatinize(word)
-        word
-    end 
+    def piglatinize(phrase)
+        phrase.split(" ").length == 1 ? piglatinize_word(phrase) : piglatinize_sentence(phrase)
+    end
 
-end 
+    def piglatinize_word(word)
+        parts = word.split(/([aeiouAEIOU].*)/)
+        start = parts[0]
+        rest = parts[1]
+
+        if start.length > 0
+            "#{rest}#{start}ay"
+        else
+            "#{rest}way"
+        end  
+    end
+
+    def piglatinize_sentence(sentence)
+        sentence.split.collect {|word| piglatinize(word)}.join(" ")      
+    end
+
+end
